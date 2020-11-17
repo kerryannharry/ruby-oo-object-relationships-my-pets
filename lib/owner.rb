cfb70748f5dafce1bbe5b61890f14e9c831e3d3a
KerryAnn Harry <kerryann.harry@yahoo.com>
2020-11-17 09:56:22 -0600

    commit tree dcaadcfe77b79c368ebf5a6e14cd329283702408

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,35 @@
 class Owner
   # code goes here
+
+    attr_reader :name, :species
+    attr_accessor :cats, :dogs
+  
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+      @species = "human"
+      @@all << self
+    end
+
+    def say_species
+      p "I am a #{@species}."
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def self.count
+      @@all.length
+    end
+  
+    def self.reset_all
+      @@all.clear
+    end
+
+    def cats
+      Cat.all.select {|cat| cat.owner != nil}.count
+    end
+
 end
